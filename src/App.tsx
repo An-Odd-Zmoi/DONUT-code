be03c448c1e3cbe8fbc6f34a116e701de0c23807
ochu761 <75078332+AoMedium@users.noreply.github.com>
2023-08-19 20:19:37 +1200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import "./App.css";
 import WorkspacePage from "./pages/WorkspacePage/WorkspacePage";
 import WorkspaceContextLayout from "./components/WorkspaceContextLayout";
 
-const ROOT_PATH = "/DONUT-code/";
-
 function App() {
   return (
     <Routes>
-      <Route path={ROOT_PATH} element={<WorkspaceContextLayout />}>
+      <Route path="/DONUT-code/" element={<WorkspaceContextLayout />}>
         <Route index element={<WorkspacePage />} />
       </Route>
     </Routes>
