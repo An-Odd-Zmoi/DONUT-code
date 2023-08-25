68672320b53951e237f529b036b407362f467d26
zzou702 <zzou702@aucklanduni.ac.nz>
2023-08-25 16:56:43 +1200

    Minor styling fix, and removed old tutorial

--- src/pages/WorkspacePage/Inspector/Panels/QuestionPanel/QuestionPanel.tsx
+++ src/pages/WorkspacePage/Inspector/Panels/QuestionPanel/QuestionPanel.tsx
@@ -158,7 +158,7 @@ function ProgramGenInfo() {
           <br />
           <b>In Summary:</b>
           <br />
-          <ol>
+          <ul>
             <li>Access and enable the tool using the provided link.</li>
             <li>
               Choose between the "Prompt" method for practice questions or the
@@ -181,126 +181,11 @@ function ProgramGenInfo() {
               Ask any questions on our discord server, and we will answer them
               the best we can!
             </li>
-            <br />
-            <br />
-            Remember, our aim is to make coding comprehension a breeze. Happy
-            practising, and best of luck with your coding journey!
-          </ol>
-          <b>*** Important Please Read ***</b>
-          <br />
-          Before we start, first thing first, due the fact that we are broke
-          part IV students, we could not obtain a proper SSL certificate, so for
-          the website to function properly, we had to use a self-signed
-          certificate.
+          </ul>
           <br />
           <br />
-          <b>
-            Please go to the following&nbsp;
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://ec2-3-24-21-254.ap-southeast-2.compute.amazonaws.com:3000"
-            >
-              Link
-            </Link>
-            , where you may see that the connection is not private.
-          </b>
-          <br />
-          <br />
-          Click on{" "}
-          <b>
-            <i>Advanced</i>
-          </b>{" "}
-          and at the bottom of your screen, click on{" "}
-          <b>
-            <i>
-              Proceed to ec2-3-24-21-254.ap-southeast-2.compute.amazonaws.com{" "}
-            </i>
-          </b>
-          . You can now close that link, it will allow you to use our website
-          properly. Apologies for the inconvenience.
-          <br />
-          <br />
-          <i>
-            If you have any questions, feel free to drop it into our&nbsp;
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://discord.gg/qGDQV4t3A"
-            >
-              Discord Channel
-            </Link>
-          </i>
-          <br />
-          <br />
-          Let's get started, and take a closer look at how everything works, and
-          don't forget to fill in the survey to help make the tool better once
-          you're done!&nbsp;&nbsp;
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://forms.gle/2trxnp6urSNq22xAA"
-          >
-            Survey ヽ(ˋДˊ)ノ
-          </Link>
-          <br />
-          <br />
-          <b>Input Options:</b> To get started, you have two options shown in
-          the code editor on the left: You can either input a prompt to generate
-          code using AI (By clicking on the “Prompt” button), or you can enter
-          your own code directly into the code editor on the left (By clicking
-          on the “Custom Code” button). Choose the method that suits you best.
-          <br />
-          <br />
-          <b>Prompt option:</b> Is there a concept you learnt in CS101 bugging
-          you? Maybe a nested loop, or is it a matrix, or file reading in
-          python? Enter them in the prompt option with your preferred language
-          E.g. nested loop Python. Provide more details in the prompt for
-          different and more personalised examples to get more practice on. E.g.
-          Generate a nested loop in python that calculates the sum of a 2D
-          array.
-          <br />
-          <br />
-          <b>
-            Not sure what prompt to to put in? Check out some of our{" "}
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://docs.google.com/document/d/1lHpkDB--OyMxE8ibWdQlXY8PF16XFAgiggyWmFuhMnw/edit?usp=sharing"
-            >
-              example prompts
-            </Link>
-          </b>
-          <br />
-          <br />
-          <b>Custom Code option:</b> Maybe there is a code snippet shown in your
-          CS101 lectures that you don’t understand, feel free to copy and paste
-          them using the custom code option.
-          <br />
-          <br />
-          <b>Questions:</b> Once you have your code ready, click the "Generate
-          Questions" button. You'll see a list of questions on the right, each
-          labelled with a difficulty level. These questions will help you assess
-          your comprehension of the code.
-          <br />
-          <br />
-          <b>Feedback and Follow-up:</b> After submitting your answers, our AI
-          will automatically evaluate them. You'll receive instant feedback on
-          your performance. If you're unsure about a question, no problem! You
-          can request explanations and even ask follow-up questions in the
-          feedback section.
-          <br />
-          <br />
-          <b>Line-by-Line Explanation:</b> While exploring your code, if you
-          encounter a line you'd like to understand better, simply go onto the
-          explanations tab, highlight the line confusing you, and click
-          “Generate Explanation”. A detailed explanation will appear, helping
-          you grasp the function of that line, as well as its purpose with
-          regards to the overall program.
-          <br />
-          <br />
-          Feel free to explore, learn, and challenge yourself with different
-          programs and prompts!
+          Remember, our aim is to make coding comprehension a breeze. Happy
+          practising, and best of luck with your coding journey!
         </Typography>
       </div>
     </Stack>
